4bd15e227858967bdcdd7f3ba5d84f0f8cba563d
Alfa-ao <godenergysoulworker@gmail.com>
2026-09-25 18:51:43 +0300

    CoreScripts WidgetCoreUserMods + AdvancedHandlersUserMods

--- Scripts/Core/AlchemyInit.lua
+++ Scripts/Core/AlchemyInit.lua
@@ -12,7 +12,7 @@ function DebugService:LogReaction( ... )
 end
 
 --------------------------------------------------------------------------------
--- Cостояние.
+-- Cостояние
 --------------------------------------------------------------------------------
 -- State кэши рецептов и т.д.
 local state = AlchemyState { messageType = CONFIG.MESSAGE_GREETINGS }
@@ -21,13 +21,11 @@ local state = AlchemyState { messageType = CONFIG.MESSAGE_GREETINGS }
 state.localization = common.GetLocalization()
 
 --------------------------------------------------------------------------------
--- Сервисы.
+-- Сервисы
 --------------------------------------------------------------------------------
--- Сервис лога. Включает вывод определенных категорий, чтобы не засорять mods.txt.
 local debugService = DebugService()
 debugService:Init { GENERAL = CONFIG.DEBUG, REACTION = CONFIG.DEBUG_REACTION }
 
--- Сервис для работы с рецептами. При первом открытии алхимки он запрашивает у игры все (250+) доступных рецептов, сохраняет их в кэш.
 local recipeService = AlchemyRecipeService()
 recipeService:Init( state )
 
@@ -35,23 +33,23 @@ local dndManager = DnDManager()
 dndManager:Init { defaultCursor = CONFIG.DND.CURSOR }
 
 --------------------------------------------------------------------------------
--- По алхимке поиск рецептов.
+-- По алхимке поиск рецептов
 --------------------------------------------------------------------------------
--- Маппер сдвигов. Когда барабаны прокручиваются, компоненты меняются. 
--- Этот класс строит карту: "если сдвинуть барабан на N, то выпадет компонент X"ю
+-- Маппер сдвигов.
+-- Строит карту: "если сдвинуть барабан на N, то выпадет компонент X"
 local drumShiftMapper = DrumShiftMapper()
 drumShiftMapper:Init( state )
 
--- Перебирает все возможные комбинации сдвигов барабанов, чтобы понять, какие рецепты вообще можно сварить.
+-- Перебирает все возможные комбинации сдвигов барабанов
 local searchAlgorithm = BacktrackingSearchAlgorithm()
 searchAlgorithm:Init( RecipeEvaluator() )
 
--- Фасад поиска. Связывает маппер и алгоритм.
+-- Сервис глубокого поиска
 local searchService = AlchemySearchService()
 searchService:Init( state, recipeService, drumShiftMapper, searchAlgorithm )
 
 --------------------------------------------------------------------------------
--- Виджеты.
+-- Виджеты
 --------------------------------------------------------------------------------
 local wtPanel = _G.mainForm:GetChildChecked( "Panel" )
 local wtOuText = wtPanel:GetChildChecked( "ouText" )
@@ -61,10 +59,10 @@ local widgetAlchemyV2 = WidgetAlchemyV2()
 widgetAlchemyV2:Init( common.GetAddonMainForm( "AlchemyV2" ) )
 
 --------------------------------------------------------------------------------
--- Всё что связано с текстом, почти.
+-- Всё что связано с текстом (почти)
 --------------------------------------------------------------------------------
 local widgetTextContainer = WidgetTextContainer { _wtTextContainer = wtOuText }
-widgetTextContainer:Init { sizePadding = CONFIG.GUI.PADDING }
+widgetTextContainer:Init()
 
 local viewService = AlchemyViewService()
 viewService:Init {
@@ -74,7 +72,7 @@ viewService:Init {
 }
 
 --------------------------------------------------------------------------------
--- Логика в событиях связано с алхимкой.
+-- Логика в событиях связано с алхимкой
 --------------------------------------------------------------------------------
 local context = {
     dnd = dndManager,
@@ -90,45 +88,60 @@ local context = {
 local alchemyEvents = AlchemyEvents()
 alchemyEvents:Init( context )
 
--- Обработчик событий (EVENT_AVATAR_*).
+-- Обработчик событий (EVENT_AVATAR_*)
 local avatarEvents = AlchemyAvatarEvents { _wtMovable = wtPanel }
 avatarEvents:Init( context )
 
 --------------------------------------------------------------------------------
--- Регистрация событий.
+-- Регистрация событий
 --------------------------------------------------------------------------------
 local events = {
     -- Алхимия
-    EVENT_ALCHEMY_STARTED = function() alchemyEvents:OnStarted() end, -- Окно алхимки открывается. Инициализируется HELLO сообщение и кэш рецептов.
-    EVENT_ALCHEMY_CANCELED = function( params ) alchemyEvents:OnCanceled( params ) end, -- Окно алхимки закрывается / вышли из варки в меню.
-    EVENT_ALCHEMY_ITEM_PLACED = function( params ) alchemyEvents:OnItemPlaced( params ) end, -- При каждом изменении слота для компонентов уведомляет, что в такой-то слот был вставлен/вынут компонент.
-    EVENT_ALCHEMY_REACTION_FINISHED = function() alchemyEvents:OnReactionFinished() end, -- Уведомляет о начале варки зелья. Хоть и название события говорит о другом...
-    EVENT_ALCHEMY_RECIPES_CHANGED = function() alchemyEvents:OnRecipesChanged() end, -- Уведомляет об необходимости обновить список рецептов.
+    { -- Окно алхимки открывается. Инициализируется HELLO сообщение и кэш рецептов.
+        function() alchemyEvents:OnStarted() end,
+        "EVENT_ALCHEMY_STARTED"
+    },
+    { -- Окно алхимки закрывается / вышли из варки в меню.
+        function( params ) alchemyEvents:OnCanceled( params ) end,
+        "EVENT_ALCHEMY_CANCELED"
+    },
+    { -- При каждом изменении слота для компонентов уведомляет, что в такой-то слот был вставлен/вынут компонент.
+        function( params ) alchemyEvents:OnItemPlaced( params ) end,
+        "EVENT_ALCHEMY_ITEM_PLACED"
+    },
+    { -- Уведомляет о начале варки зелья. Хоть и название события говорит о другом...
+        function() alchemyEvents:OnReactionFinished() end,
+        "EVENT_ALCHEMY_REACTION_FINISHED"
+    },
+    { -- Уведомляет об необходимости обновить список рецептов.
+        function() alchemyEvents:OnRecipesChanged() end,
+        "EVENT_ALCHEMY_RECIPES_CHANGED"
+    },
     
     -- Аватар
-    EVENT_AVATAR_CREATED = function() avatarEvents:OnAvatarCreated() end, -- Инициализация логики. Когда игрок уже в игре, ТОЛЬКО ТОГДА необходимо применинить следующую логику.
-    EVENT_AVATAR_ITEM_TAKEN = function( params ) avatarEvents:OnItemTaken( params ) end, -- Всё что попало в сумку игрока от крафта алхимки.
-
-    -- Обновляет размеры Panel при изменении масштаба/размера окна игры.
-    -- Причина:
-    -- 1) Закомментить этот ивент.
-    -- 2) В игре - Меню -> Графика -> выставить Режим: "Оконный"
-    -- 3) Изменять размер окна игры.
-    -- Результат: Часть текста смещается и скрывается, а padding отступы превращаются невалидными.
-    -- DnDManager не знает как работать с текстовым контейнером, если смотреть на опцию padding.
-    EVENT_POS_CONVERTER_CHANGED = function()
-        local exactHeight = widgetTextContainer:GetExactTextHeight()
-        widgetTextContainer:UpdateSizePanel( exactHeight )
-    end,
+    { -- Инициализация логики. Когда игрок уже в игре, только тогда необходимо применинить следующую логику.
+        function() avatarEvents:OnAvatarCreated() end,
+        "EVENT_AVATAR_CREATED"
+    },
+    { -- Всё что попало в сумку игрока от крафта алхимки.
+        function( params ) avatarEvents:OnItemTaken( params ) end,
+        "EVENT_AVATAR_ITEM_TAKEN"
+    },
+    
+    -- Позицирование
+    { -- Обновляет размеры Panel при изменении масштаба/размера окна игры.
+        function()
+            local exactHeight = widgetTextContainer:GetExactTextHeight()
+            widgetTextContainer:UpdateSizePanel( exactHeight )
+        end,
+        "EVENT_POS_CONVERTER_CHANGED"
+    },
 }
 
-for eventName, handler in pairs( events ) do
-    common.RegisterEventHandler( handler, eventName )
-end
+advEvent.RegisterEventHandlers( false, events )
 
 --------------------------------------------------------------------------------
--- Если аддон был перезагружен (reload) в момент, когда аватар уже находится в игре, событие EVENT_AVATAR_CREATED повторно не сработает.
---------------------------------------------------------------------------------
+
 if avatar.IsExist() then
     avatarEvents:OnAvatarCreated()
 end
